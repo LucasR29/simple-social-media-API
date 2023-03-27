5058c6bf720232c55194f81cffdf61ef44ee7f12
LucasR <lrigon2904@gmail.com>
2023-03-26 23:12:40 -0300

    first commit

---
+++ posts/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+import uuid
+
+
+class Post(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+    user = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE, related_name="posts", default=None
+    )
+
+    content = models.TextField()
+    public = models.BooleanField()
+
+    likes = models.ManyToManyField("users.User", related_name="posts_liked")
+    posted_at = models.DateTimeField(auto_now_add=True)
+
+
+class PostComments(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+
+    post = models.ForeignKey(
+        "posts.Post",
+        on_delete=models.CASCADE,
+        related_name="post_comments_user",
+    )
+
+    comment_user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="user_post_comments",
+    )
+
+    text = models.TextField()
+    commented_at = models.DateTimeField(auto_now_add=True)
---
+++ posts/views.py
@@ -0,0 +1,89 @@
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import generics
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from followers.models import FollowersList
+from friends.models import FriendList
+
+from posts.permissions import IsAccountOrFriend
+from users.models import User
+from .models import Post, PostComments
+from .serializers import PostCommentsSerializer, PostSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from django.db.models import Q
+
+
+class PostView(generics.ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        return Post.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class PostTimeLineView(generics.ListAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        user = User.objects.get(id=self.request.user.id)
+        print(user)
+        user_follow = user.following.all().values_list("owner")
+        friends_list = FriendList.objects.get(owner=self.request.user)
+        friends = friends_list.friends.all()
+        posts_friends = Post.objects.filter(
+            Q(user__in=friends) | Q(user__in=user_follow)
+        )
+
+        return posts_friends[::-1]
+
+
+class PostUniqueView(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    lookup_url_kwarg = "post_id"
+
+    def like_post(self, request, post_id):
+        post = get_object_or_404(Post, id=post_id)
+        post.likes.add(request.user)
+
+    def unlike_post(self, request, post_id):
+        post = get_object_or_404(Post, id=post_id)
+        post.likes.remove(request.user)
+
+    def put(self, request, *args, **kwargs):
+        if request.data.get("action") == "like":
+            return self.like_post(request, *args, **kwargs)
+        elif request.data.get("action") == "unlike":
+            return self.unlike_post(request, *args, **kwargs)
+        else:
+            return super().put(request, *args, **kwargs)
+
+
+class PostCommentCreate(generics.CreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAccountOrFriend]
+
+    queryset = PostComments.objects.all()
+    serializer_class = PostCommentsSerializer
+    lookup_url_kwarg = "post_id"
+
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, id=self.kwargs["post_id"])
+        serializer.save(comment_user=self.request.user, post=post)
+
+
+class PostCommentDestroy(generics.DestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    queryset = PostComments.objects.all()
+    serializer_class = PostCommentsSerializer
+    lookup_url_kwarg = "comment_id"
